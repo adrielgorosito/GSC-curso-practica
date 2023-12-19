065022bf62b6d719981be68f989e9950068e1368
Adriel Gorosito <adrielgorosito14@gmail.com>
2023-12-19 13:10:41 -0300

    Clase 21-11

---
+++ Prácticas NET/Clase 13 - 21-11 - Entity Framework/EjEfCore/EjEfCore/DataAccess/AcademiaDbContext.cs
@@ -0,0 +1,33 @@
+﻿using EjEfCore.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EjEfCore.DataAccess
+{
+    public class AcademiaDbContext : DbContext
+    {
+        // Cada vez que se instancie el DbContext, checkear que la base de datos esté creada
+        public AcademiaDbContext() => this.Database.EnsureCreated();
+
+        public DbSet<Estudiante> Estudiantes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=AcademiaDb");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // De la siguiente forma se determina a "Legajo" como PK
+            // Además, se deshabilita la auto-generación numérica
+            /* modelBuilder.Entity<Estudiante>(e => 
+            {
+                e.HasKey(p => p.Legajo);
+                e.Property(p => p.Legajo).ValueGeneratedNever();
+            }); */
+
+            modelBuilder.Entity<Estudiante>()
+                .Property(e => e.Nombre)
+                .HasMaxLength(50);
+        }
+    }
+}
---
+++ Prácticas NET/Clase 13 - 21-11 - Entity Framework/EjEfCore/EjEfCore/Program.cs
@@ -0,0 +1,70 @@
+﻿using EjEfCore.DataAccess;
+using EjEfCore.Domain;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+
+using var context = new AcademiaDbContext();
+
+await Create(context);
+await ReadAll(context);
+
+int id = await Create(context);
+await Update(context, id);
+await ReadAll(context);
+
+await Delete(context, 6);
+
+static async Task<int> Create(AcademiaDbContext context)
+{
+    var estudiante = new Estudiante()
+    {
+        Nombre = "Juan",
+        Apellido = "Perez",
+        FechaNacimiento = new DateOnly(2001, 6, 1),
+        Legajo = 12345
+    };
+    context.Add(estudiante);
+    await context.SaveChangesAsync();
+
+    return estudiante.Id;
+}
+
+static async Task ReadAll(AcademiaDbContext context)
+{
+    var estudiantes = await context.Estudiantes.ToListAsync();
+
+    // Para mejorar la legibilidad del Json que se imprime
+    var options = new JsonSerializerOptions()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = true,
+    };
+
+    estudiantes.ForEach(e =>
+    {
+        string json = System.Text.Json.JsonSerializer.Serialize(e, options);
+        Console.WriteLine(json);
+    });
+}
+
+static async Task Update(AcademiaDbContext context, int id)
+{
+    var estudiante = await context.FindAsync<Estudiante>(id);
+
+    estudiante.FechaNacimiento = new DateOnly(2003, 6, 1);
+
+    // context.Update(estudiante);
+    // En este caso, no es necesario el update ya que ya poseemos el estudiante
+    // de la instancia context, y dicha instancia no cambia
+    await context.SaveChangesAsync();
+}
+
+static async Task Delete(AcademiaDbContext context, int id)
+{
+    var estudiante = await context.FindAsync<Estudiante>(id);
+
+    context.Remove(estudiante);
+
+    await context.SaveChangesAsync();
+}
